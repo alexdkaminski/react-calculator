6fd3bfb114d062f5c14cc9b83ac187e221ca03fe
Alex Kaminski <alexdkaminski@gmail.com>
2020-07-25 16:44:09 +1000

    User story #13 complete

--- src/App.js
+++ src/App.js
@@ -23,12 +23,28 @@ function App() {
     return a - b
   }
 
+  const isOperator = (a) => {
+    if (a === 'x' || a === '/' || a === '+' || a === '-') {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const isNegative = (a) => {
+    if (a === '-') {
+      return true
+    } else {
+      return false
+    }
+  }
+
   const handleNumberClick = (e) => {
     const number = e.target.textContent
     const lastElement = display[display.length - 1]
-    if (display[0] === '0') {
+    if (display.length === 1 && display[0] === '0') {
       setDisplay([number])
-    } else if (lastElement === '+' || lastElement === '-' ||lastElement === 'x' ||lastElement === '/') {
+    } else if (isOperator(lastElement)) {
       setDisplay(display.concat(number))
     }
     else {
@@ -44,27 +60,44 @@ function App() {
     let result
     if (operator === '=') {
       // Evaluate formula
-      let numberArray = display.map(element =>
-        element >= 0 ? parseInt(element) : element
-      )
+      let numberArray = display.map(element => {
+        if (element.includes('.')) {
+          return parseFloat(element)
+        } else if (element >= 0) {
+          return parseInt(element)
+        } else {
+          return element
+        }
+      })
 
       // Loop through checking for multiplication
       for (let i = numberArray.length - 1; i >= 0; i--) {
         if (numberArray[i] === 'x') {
           console.log('Multiplication detected')
-          // If we already have a result, multiply result by second number
-          // if (result) {
-          //   console.log(`We have a result, multiplying ${result} by ${numberArray[i+1]}`)
-          //   result = multiply(result,numberArray[i+1])
-          // // If we don't have a result, multiply first number by second number
-          // } else {
-          console.log(`Multiplying ${numberArray[i-1]} by ${numberArray[i+1]}`)
-          result = multiply(numberArray[i-1],numberArray[i+1])
-
-          console.log(`Result: ${result}`)
-          console.log(`Old number array: ${numberArray}`)
-          numberArray.splice(i-1,3,result)
-          console.log(`New number array: ${numberArray}`)
+          // Check for preceding operator
+          if (isOperator(numberArray[i-1])) {
+            console.log(`Preceding operator found, multiplying ${numberArray[i-2]} by ${numberArray[i+1]}`)
+            result = multiply(numberArray[i-2],numberArray[i+1])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-2,4,result)
+            console.log(`New number array: ${numberArray}`)
+          } else if (isNegative(numberArray[i+1])) {
+            numberArray[i+2] = -Math.abs(numberArray[i+2])
+            console.log('Negative detected')
+            console.log(numberArray[i+2])
+            result = multiply(numberArray[i-1],numberArray[i+2])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-3,4,result)
+            console.log(`New number array: ${numberArray}`)
+          } else if (!isOperator(numberArray[i+1])) {
+            console.log(`Multiplying ${numberArray[i-1]} by ${numberArray[i+1]}`)
+            result = multiply(numberArray[i-1],numberArray[i+1])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-1,3,result)
+          }
         }
       }
 
@@ -72,15 +105,31 @@ function App() {
       for (let i = numberArray.length - 1; i >= 0; i--) {
         if (numberArray[i] === '/') {
           console.log('Division detected')
-
-          // If we already have a result, divide result by second number
-          console.log(`We don't have a result, dividing ${numberArray[i-1]} by ${numberArray[i+1]}`)
-          result = divide(numberArray[i-1],numberArray[i+1])
-
-          console.log(`Result: ${result}`)
-          console.log(`Old number array: ${numberArray}`)
-          numberArray.splice(i-1,3,result)
-          console.log(`New number array: ${numberArray}`)
+          // Check for preceding operator
+          if (isOperator(numberArray[i-1])) {
+            console.log(`Dividing ${numberArray[i-2]} by ${numberArray[i+1]}`)
+            result = divide(numberArray[i-2],numberArray[i+1])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-2,4,result)
+            console.log(`New number array: ${numberArray}`)
+          } else if (isNegative(numberArray[i+1])) {
+            numberArray[i+2] = -Math.abs(numberArray[i+2])
+            console.log('Negative detected')
+            console.log(numberArray[i+2])
+            result = divide(numberArray[i-1],numberArray[i+2])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-3,4,result)
+            console.log(`New number array: ${numberArray}`)
+          } else if (!isOperator(numberArray[i+1])) {
+            console.log(`Dividing ${numberArray[i-1]} by ${numberArray[i+1]}`)
+            result = divide(numberArray[i-1],numberArray[i+1])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-1,3,result)
+            console.log(`New number array: ${numberArray}`)
+          }
         }
       }
 
@@ -88,39 +137,69 @@ function App() {
       for (let i = numberArray.length - 1; i >= 0; i--) {
         if (numberArray[i] === '+') {
           console.log('Addition detected')
-          // If we already have a result, add second number to result
-          // if (result) {
-          //   console.log(`We have a result, adding ${result} to ${numberArray[i+1]}`)
-          //   result = add(result,numberArray[i+1])
-          // // If we don't have a result, add first number to second number
-          // } else {
-            console.log(`We don't have a result, adding ${numberArray[i-1]} to ${numberArray[i+1]}`)
+          // Check for preceding operator
+          if (isOperator(numberArray[i-1])) {
+            console.log(`Adding ${numberArray[i-2]} to ${numberArray[i+1]}`)
+            result = add(numberArray[i-2],numberArray[i+1])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-2,4,result)
+            console.log(`New number array: ${numberArray}`)
+          } else if (isNegative(numberArray[i+1])) {
+            numberArray[i+2] = -Math.abs(numberArray[i+2])
+            console.log('Negative detected')
+            console.log(numberArray[i+2])
+            result = add(numberArray[i-1],numberArray[i+2])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-3,4,result)
+            console.log(`New number array: ${numberArray}`)
+          } else if (!isOperator(numberArray[i+1])) {
+            console.log(`Adding ${numberArray[i-1]} to ${numberArray[i+1]}`)
             result = add(numberArray[i-1],numberArray[i+1])
-
             console.log(`Result: ${result}`)
             console.log(`Old number array: ${numberArray}`)
             numberArray.splice(i-1,3,result)
             console.log(`New number array: ${numberArray}`)
+          }
         }
       }
       // Loop through checking for subtraction
       for (let i = numberArray.length - 1; i >= 0; i--) {
         if (numberArray[i] === '-') {
           console.log('Subtraction detected')
-          // If we already have a result, subtract second number from result
-          // if (result) {
-          //   console.log(`We have a result, subtracting ${numberArray[i+1]} from ${result}`)
-          //   result = subtract(result,numberArray[i+1])
-          // // If we don't have a result, subtract the second number from the first number
-          // } else {
-            console.log(`We don't have a result, subtracting ${numberArray[i+1]} from ${numberArray[i-1]}`)
+          console.log(numberArray)
+          console.log(numberArray[i+1])
+          // Check if negative
+          if (isNegative(numberArray[i-1])) {
+            console.log(2)
+            numberArray[i+1] = -Math.abs(numberArray[i+1])
+            console.log('Negative detected')
+            console.log(numberArray[i+1])
+            console.log(`Subtracting ${numberArray[i+1]} from ${numberArray[i-2]}`)
+            result = subtract(numberArray[i-2],numberArray[i+1])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-3,4,result)
+            console.log(`New number array: ${numberArray}`)
+          // Check for preceding operator
+          } else if (isOperator(numberArray[i-1])) {
+            console.log(1)
+            console.log(`Subtracting ${numberArray[i+1]} from ${numberArray[i-2]}`)
+            result = subtract(numberArray[i-2],numberArray[i+1])
+            console.log(`Result: ${result}`)
+            console.log(`Old number array: ${numberArray}`)
+            numberArray.splice(i-2,4,result)
+            console.log(`New number array: ${numberArray}`)
+          } else if (!isOperator(numberArray[i+1])) {
+            console.log(3)
+            console.log(`Subtracting ${numberArray[i+1]} from ${numberArray[i-1]}`)
             result = subtract(numberArray[i-1],numberArray[i+1])
-
-
             console.log(`Result: ${result}`)
             console.log(`Old number array: ${numberArray}`)
             numberArray.splice(i-1,3,result)
             console.log(`New number array: ${numberArray}`)
+          }
         }
       }
 
@@ -129,11 +208,17 @@ function App() {
   }
 
   const handleDecimalClick = (e) => {
+    const lastElement = display[display.length - 1]
+
+    if (lastElement !== '+' || lastElement !== '-' ||lastElement !== 'x' ||lastElement !== '/') {
+      const newArray = [...display]
+      newArray[[newArray.length - 1]] += '.'
+      setDisplay(newArray)
+    }
   }
 
   const handleClearClick = () => {
     setDisplay(['0'])
-    // setOperation(null)
   }
 
   return (
